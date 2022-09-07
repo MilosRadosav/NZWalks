28338adc20dde6c391d77e6e9b2ed7db8614eadf
Milos <misa90etf@gmail.com>
2022-09-07 09:17:49 +0200

    End of section 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string? Name{ get; set; }
+        public double? Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation property
+        public virtual Region? Region { get; set; }
+        public virtual WalkDifficulty? WalkDifficulty { get; set; }
+
+    }
+}
